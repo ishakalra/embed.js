2ea8132a9132b2555a7cd20537981d22ea3c5a95
ishakalra <isha16152@iiitd.ac.in>
2017-03-11 15:03:24 +0530

    Update utils.test.js

--- test/modules/utils.test.js
+++ test/modules/utils.test.js
@@ -127,15 +127,4 @@ test('createText() method',t => {
 		const expected3= 'Hello world';
 		t.same(actual3, expected3);
 	
-		const str4 = '';
-		const embeds4 = [ { index: 13, text: 'me' }, { index: 4, text: 'this' }, { index: 9, text: 'is'} ];
-		const actual4 = createText(str4, embeds4);
-		const expected4= ' this is me';
-		t.same(actual4, expected4);
-	
-		const str5 = '';
-		const embeds5 = [];
-		const actual5 = createText(str5, embeds5);
-		const expected5= '';
-		t.same(actual5, expected5);
 });
